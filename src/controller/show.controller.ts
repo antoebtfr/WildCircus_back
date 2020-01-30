ea11ddd7027a60fa9ef8b7a1a338cac645ccdd4a
cdhbze93 <antoebt@hotmail.com>
2020-01-30 17:27:28 +0100

    feat: CRUD shows

--- src/controller/show.controller.ts
+++ src/controller/show.controller.ts
@@ -2,15 +2,25 @@ import { ShowService } from './../service/show.service';
 import { Application, Request, Response, Router } from 'express';
 
 export const showController = (app: Application) => {
-    const user = new ShowService();
+    const service = new ShowService();
     const router = Router();
 
-    router.get("/", async (req: Request, res: Response) => {
-        res.send(await user.get());
+    router.get('/', async (req: Request, res: Response) => {
+        res.send(await service.get());
     });
 
     router.post('/', async (req: Request, res: Response) => {
-        res.send(await user.post(req.body));
+        res.send(await service.post(req.body));
+    });
+
+    router.put('/:id', async (req: Request, res: Response) => {
+        await service.modifyById(Number(req.params.id), req.body);
+        res.send('La modificattion bien été effectuée').status(200);
+    })
+
+    router.put('/:id', async (req: Request, res: Response) => {
+        await service.deleteById(Number(req.params.id));
+        res.send('La suppression a bien été effectuée').status(200);
     })
 
     app.use('/shows', router)
--- src/service/show.service.ts
+++ src/service/show.service.ts
@@ -1,5 +1,5 @@
 import { ShowRepository } from './../repository/show.repository';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getConnection, ObjectLiteral } from 'typeorm';
 import { Show } from '../entity/show.entity';
 
 export class ShowService {
@@ -11,5 +11,13 @@ export class ShowService {
 
     post(show: Show) {
         return this.repo.save(show)
+    }
+
+    modifyById(id: number, body: any) {
+        return this.repo.update({id}, body)
+    }
+
+    deleteById(id:number){
+        return this.repo.delete(id)
     }
 }
